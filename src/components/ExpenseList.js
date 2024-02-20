301a04d36a11002c636c6cb937360b41b93bac54
laguenat000 <124487677+laguenat000@users.noreply.github.com>
2024-02-20 12:59:46 -0600

    Personalized ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -9,7 +9,7 @@ const ExpenseList = () => {
         <table className='table'>
               <thead className="thead-light">
             <tr>
-              <th scope="col">Department</th>
+              <th scope="col">Category</th>
               <th scope="col">Allocated Budget</th>
               <th scope="col">Increase by 10</th>
               <th scope="col">Delete</th>
